16a945c3cbc5ef6cadcca46d598e7e4d6b0ec05b
Jorge <glph8911@gmail.com>
2024-02-27 11:45:03 -0500

    completing CRUD

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v2');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
